7d4a67a00fcfc46fab9f63f7f4e252cae21edbf9
Ptashkina Liliya <efekta9@gmail.com>
2018-09-21 19:03:53 +0300

    no message

--- js/script.js
+++ js/script.js
@@ -7,39 +7,39 @@ $(document).ready(function() {
 });
 
 //slider
-$(document).ready(function() {
-        $('.speakers-slider').slick({
-        	arrows: false,
-          dots: false,
-          infinite: false,
-          speed: 300,
-          slidesToShow: 6,
-				  slidesToScroll: 1,
-				  autoplay: true,
-				  autoplaySpeed: 1600,
-          responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-          ]
-        });
-});
+// $(document).ready(function() {
+//         $('.speakers-slider').slick({
+//         	arrows: false,
+//           dots: false,
+//           infinite: false,
+//           speed: 300,
+//           slidesToShow: 6,
+// 				  slidesToScroll: 1,
+// 				  autoplay: true,
+// 				  autoplaySpeed: 1600,
+//           responsive: [
+//             {
+//               breakpoint: 1024,
+//               settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 3,
+//                 infinite: true,
+//               }
+//             },
+//             {
+//               breakpoint: 600,
+//               settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 2
+//               }
+//             },
+//             {
+//               breakpoint: 480,
+//               settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1
+//               }
+//             }
+//           ]
+//         });
+//});
