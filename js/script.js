47fd7e2b61c73881e00d37df28799df968366593
Ptashkina Liliya <efekta9@gmail.com>
2018-09-21 11:11:07 +0300

    no message

--- js/script.js
+++ js/script.js
@@ -5,3 +5,41 @@ $(document).ready(function() {
     }
     $('input[name="phone"]').mask("+7(999) 999-9999");
 });
+
+//slider
+$(document).ready(function() {
+        $('.speakers-slider').slick({
+        	arrows: false,
+          dots: false,
+          infinite: false,
+          speed: 300,
+          slidesToShow: 6,
+				  slidesToScroll: 1,
+				  autoplay: false,
+				  autoplaySpeed: 2000,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        });
+});
